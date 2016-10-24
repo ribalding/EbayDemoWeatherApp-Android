afe0806fcdf952df9bc347fff8c92b95c88263b8
rh <ryanjharvey88@gmail.com>
2016-10-24 16:11:32 -0700

    Added ButterKnife for data binding.  Will now apply BioRhyme font to the Main Activity title at runtime.

--- app/src/main/java/ryanharvey/ebaydemoproject/MainActivity.java
+++ app/src/main/java/ryanharvey/ebaydemoproject/MainActivity.java
@@ -1,13 +1,26 @@
 package ryanharvey.ebaydemoproject;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.mainActivityButton) Button mainActivityButton;
+    @Bind(R.id.mainActvityTitleTextView) TextView mainActivityTitleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface bioRhymeFont = Typeface.createFromAsset(getAssets(), "fonts/BioRhyme-Regular.ttf");
+        mainActivityTitleTextView.setTypeface(bioRhymeFont);
     }
 }
