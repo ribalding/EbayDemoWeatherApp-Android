a787cc14e4615bef72efde9e6219a61dd69ca75c
rh <ryanjharvey88@gmail.com>
2016-10-24 16:32:29 -0700

    Will now start Weather Display Activity on Weather Display button click

--- app/src/main/java/ryanharvey/ebaydemoproject/MainActivity.java
+++ app/src/main/java/ryanharvey/ebaydemoproject/MainActivity.java
@@ -1,15 +1,17 @@
 package ryanharvey.ebaydemoproject;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Bind(R.id.mainActivityButton) Button mainActivityButton;
     @Bind(R.id.mainActvityTitleTextView) TextView mainActivityTitleTextView;
@@ -20,7 +22,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        //Apply Bio Rhyme font to title text view at run time
         Typeface bioRhymeFont = Typeface.createFromAsset(getAssets(), "fonts/BioRhyme-Regular.ttf");
         mainActivityTitleTextView.setTypeface(bioRhymeFont);
+
+        mainActivityButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mainActivityButton){
+            //On Weather Display button click, start Weather Display Activity
+            Intent weatherDisplayIntent = new Intent(MainActivity.this, WeatherDisplayActivity.class);
+            startActivity(weatherDisplayIntent);
+        }
     }
 }
---
+++ app/src/main/java/ryanharvey/ebaydemoproject/WeatherDisplayActivity.java
@@ -0,0 +1,13 @@
+package ryanharvey.ebaydemoproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_display);
+    }
+}
